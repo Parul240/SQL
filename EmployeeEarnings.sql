e4693204b6af1a8dbea792a4798dea44fe7b2d5e
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-07 17:06:35 +0530

    Level : Easy

---
+++ EmployeeEarnings.sql
@@ -0,0 +1,8 @@
+/* https://www.hackerrank.com/challenges/earnings-of-employees */
+
+
+SELECT months*salary, COUNT(*) 
+FROM Employee
+WHERE months*salary >= 
+(SELECT MAX(salary * months) 
+FROM Employee)
