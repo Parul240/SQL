c854d31d3b5eb53c5cb3d88830612e6108bfcf16
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-03-21 18:17:18 +0530

    Level : Easy

--- 182. Duplicate_Emails.sql
+++ 182. Duplicate_Emails.sql
@@ -13,7 +13,7 @@ id is the primary key column for this table.
 Each row of this table contains an email. The emails will not contain uppercase letters.
 
 
-Write an SQL query to report all the duplicate emails. Note that it is guaranteed that the email field is not NULL.
+Write an SQL query to report all the duplicate emails. Note that its guaranteed that the email field is not NULL.
 
 Return the result table in any order.
 
